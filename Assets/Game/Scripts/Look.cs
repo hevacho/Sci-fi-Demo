7e3724b7e8d8d5f568ae9f78322fe3cbd8c00939
Rodrigo Guizán Carballeira <hevachoProyectos@gmail.com>
2020-05-27 13:41:50 +0200

    Player character

---
+++ Assets/Game/Scripts/Look.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Look : MonoBehaviour
+{
+    [SerializeField]
+    private float _sensitivity = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        Vector3 newRotation = transform.localEulerAngles;
+        newRotation.y += mouseX * _sensitivity;
+
+        float mouseY = Input.GetAxis("Mouse Y");
+        newRotation.x -= mouseY * _sensitivity;
+
+        transform.localEulerAngles = newRotation;
+    
+    }
+}
---
+++ Assets/Game/Scripts/Player.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float _speed=3.5f;
+    private float _gravity = -9.81f;
+
+    private CharacterController _controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+        Vector3 velocity = direction * _speed;
+        
+        //transform to worldSpace
+        velocity = transform.transform.TransformDirection(velocity);
+
+        //gravity always in worlSpace coordenates
+        velocity.y = _gravity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
